0e640144a669f3f132c3774586996797fa761cce
Rushikesh rapashe <117022752+Rushi2421@users.noreply.github.com>
2023-10-18 23:26:16 +0530

    Update OTP_version2.py

--- OTP_version2.py
+++ OTP_version2.py
@@ -1,75 +1,31 @@
-from twilio.rest import Client
 import random
-import sms
 import smtplib
-
-client = Client(sms.account_sid, sms.Auth_token)
-
-def generate_OTP():                                   # generate OTP
+import re
+def generate_OTP():
     global otp
     otp= ''.join([str(random.randint(0, 9)) for i in range(6)])
 
-fotp=generate_OTP()
+def send_otp():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login('rushirapashe11@gmail.com', 'qzlxjfftbfieqwmu')
+    msg = 'HELLO YOUR OTP IS ' + str(otp)
+    server.sendmail('rushirapashe11@gmail.com', s, msg)
+    server.quit()
 
-def validate_mobile(num):                             # validating mobile numbebr
-    return len(num) == 10 and num.isdigit()
+def validiate_email(s):
 
-def send_otp_over_mobile(target_no):                  #send OTP On SMS
-    if (validate_mobile(target_no)):
-        target_no = "+91" + target_no
-        message = client.messages.create(
-            body="\nYour OTP is "+str(otp),
-            from_=sms.twilio_no,
-            to=sms.target_no
-        )
-        print(message.body)
+  pat = "^[a-zA-Z0-9-_]+@[a-zA-Z0-9]+\.[a-z]{1,3}$"
+  if re.match(pat, s):
+        print('valid email')
+  else:
+        print('invalid email')
 
-    else:
-        print("INVALID MOBILE NUMBER!")
-        target_no = input("ENTER AGAIN: ")
-        validate_mobile(target_no)
-        send_otp_over_mobile(target_no)
+global s
+s= input('Enter your email address\n')
 
 
 
-def validate_email(mail):                              # validiate email id
-    if "@" not in mail and "." not in mail:
-        return False
-    else:
-        return True
-
-
-
-def send_otp(mail):                                     #send OTP via Email
-    if (validate_email(mail)):
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login('rushirapashe11@gmail.com', 'qzlxjfftbfieqwmu')
-        msg = 'HELLO YOUR OTP IS ' + str(otp)
-        server.sendmail('rushirapashe11@gmail.com', mail, msg)
-        server.quit()
-        print("OTP sent! ")
-        print("Your OTP is",str(otp))
-    else:
-        print("INVALID MAIL!")
-        mail = input("ENTER AGAIN: ")
-        validate_email(mail)
-        send_otp(mail)
-
-
-#__Menu__
-
-print("\n========================================Welcome to generate OTP========================================")
-print("\n                                   <------CHOOSE PLATFORM------>                              \n\n1.SMS:\n2.Email:")
-ans = input("\nENTER YOUR CHOICE:\n")
-
-if (ans.lower() == "1"):
-
-    number = input("ENTER YOUR MOBILE NUMBER: ")
-    validate_mobile(number)
-    send_otp_over_mobile(number)
-
-elif (ans.lower() == "2"):
-
-    recipent = input("ENTER YOUR MAIL ID:\n ")
-    send_otp(recipent)
+print(generate_OTP())
+print(validiate_email(s))
+send_otp()
